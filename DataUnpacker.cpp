f1fc6544ca4f3dc70eec4fac00dd6a406fed9c98
OHall <oscar.hall@ed.ac.uk>
2017-12-05 17:43:30 +0000

    Updates to the unpacker stage

--- DataUnpacker.cpp
+++ DataUnpacker.cpp
@@ -3,6 +3,53 @@
 
 DataUnpacker::DataUnpacker(){};
 
+UnpackedDataItem::UnpackedDataItem(std::pair < unsigned int, unsigned int> inData){
+
+	dataType = 3;
+	unsigned int chIdentity = (inData.first >> 16) & 0xFFF; //Word 0, bits 16:27
+	fee64ID = (chIdentity >> 6) & 0x003F;  					//Top 6 bit of chIdentity (22:27) are FEE64 number
+	channelID = chIdentity & 0x003f;						//Bottom 6 bits of chIdentity (16:21) are channel number
+	adcRange = (inData.first >> 28 ) & 0x0001;				//Bit 28 - Veto bit used as ADC range
+	adcData = inData.first & 0xFFFF; 						//Bits 0:15 of Word 0 is ADC data
+	timeStampLSB = inData.second & 0x0FFFFFFF;				//Word 1, bits 0:27 - Timestamp LSB
+
+}
+
+InformationDataItem::InformationDataItem(std::pair < unsigned int, unsigned int> inData){
+
+	dataType = 2;
+
+	infoField = ( inData.first & 0x000FFFFF); 				//Word 0, bits 0:19 is the information field
+	infoCode = (inData.first >> 20) & 0x000F;				//Word 0, bits 20:23 - Information code
+	fee64ID = (inData.first >> 24) & 0x003F;				//Word 0, bits 24:29 - FEE64 module number
+	timeStampLSB = (inData.second) & 0x0FFFFFFF;			//Word 1, bits 0:27 - Timestamp LSB
+
+	if ( infoCode == 2 || infoCode == 3 || infoCode == 4){
+		timeStampMSB = infoField;
+	}
+
+	if ( infoCode == 8){
+
+		corrScalerIndex = (infoFiled & 0x000F0000) >> 16;	//Index of correlation scaler
+		corrScalerTimeStamp = (infoField & 0x0000FFFF);		//Bits with timestamp
+	}
+
+
+}
+
+InformationDataItem::GetTimeStampMSB(){
+	return TimeStampMSB;
+}
+InformationDataItem::GetInfoCode(){
+	return infoCode;
+}
+
+InformationDataItem::GetCorrScalerIndex(){
+	return corrScalerIndex;
+}
+InformationDataItem::GetCorrScalerTimeStamp(){
+	return corrScalerTimeStamp;
+}
 void DataUnpacker::BeginDataUnpacker(DataReader & dataReader){
 
 	for(;;){
@@ -15,5 +62,48 @@ void DataUnpacker::BeginDataUnpacker(DataReader & dataReader){
 }
 
 void DataUnpacker::UnpackWords(std::pair < unsigned int, unsigned int> wordsIn){
-	
+	//Takes the data Words that have been read in and determines the data type and
+	//unpacks the data accordingly.
+
+	dataType = ( (wordsIn.first >>30) & 0x3);
+
+	if (dataType ==3 ){
+		//ADC data item - Unpack into ADCDataItem format
+		adcDataItem ADCDataItem(wordsIn);
+
+		
+	}
+	if (dataType == 2){
+
+		informationDataItem InformationDataItem(wordsIn);
+
+		if(infomationDataItem.GetInfoCode == 2){
+			timeStampMSB = informationDataItem.GetTimeStampMSB();
+			timeStampMSBStatus == true;
+
+			#ifdef DEB_UNPACKER
+				std::cout << "Timestamp MSB Updated - " << TimeStampMSB << " PAUSE information item"
+			#endif
+		}
+		else if(infomationDataItem.GetInfoCode == 3){
+			timeStampMSB = informationDataItem.GetTimeStampMSB();
+			timeStampMSBStatus == true;
+
+			#ifdef DEB_UNPACKER
+				std::cout << "Timestamp MSB Updated - " << TimeStampMSB << " RESUME information item"
+			#endif
+		}
+		else if(infomationDataItem.GetInfoCode == 4){
+			timeStampMSB = informationDataItem.GetTimeStampMSB();
+			timeStampMSBStatus == true;
+
+			#ifdef DEB_UNPACKER
+				std::cout << "Timestamp MSB Updated - " << TimeStampMSB << " SYNC100 information item"
+			#endif
+		}
+		else if(informationDataItem.GetInfoCode() == 8){
+
+		}
+
+	}
 }
--- DataUnpacker.hpp
+++ DataUnpacker.hpp
@@ -7,6 +7,46 @@
 
 //class DataReader; //Let DataUnpacker know about the existence of DataReader so it can access it
 
+class ADCDataItem{
+
+	private:
+
+		unsigned long timeStamp;    //Internal AIDA time stamp
+		unsigned long timeStampLSB; // Least significant bits of timestamp
+
+		unsigned int adcData;       //ADC value
+		unsigned int sampleLength;
+
+		unsigned char dataType;     // Data type: 0 = Sample waveform, 1 = Sample Length, 2 = Info Data, 3 = ADC Data
+		unsigned char fee64ID;      //FEE64 ID
+		unsigned char channelID;	// Channel ID
+		unsigned char adcRange;		// ADC range: 0 = low energy, 1 = high energy
+
+	public:
+		ADCDataItem::ADCDataItem(std::pair < unsigned int, unsigned int> inData);
+};
+
+class InformationDataItem{
+
+	private:
+
+		unsigned long timeStamp;    		//Internal AIDA time stamp
+		unsigned long timeStampLSB; 		// Least significant bits of timestamp
+		unsigned long timeStampMSB;			// Most significant bits of timestamp
+		unsigned long infoField;    		//Iformation field of AIDA data word
+
+		unsigned int sampleLength;
+
+		unsigned char dataType;     		// Data type: 0 = Sample waveform, 1 = Sample Length, 2 = Info Data, 3 = ADC Data
+		unsigned char fee64ID;      		//FEE64 ID
+		unsigned char infoCode;				//Info code of AIDA "information" word
+		unsigned char corrScalerIndex;		//Index of correlation scaler
+		unsigned char corrScalerTimeStamp;	//Timestamp of correlation scaler
+
+	public:
+		InformationDataItem::InformationDataItem(std::pair < unsigned int, unsigned int> inData);
+};
+
 class DataUnpacker{
 
 	private:
@@ -14,12 +54,31 @@ class DataUnpacker{
 		std::pair < unsigned int, unsigned int> dataWords;
 		unsigned int word0, word1;
 
+		unsigned long correlationScalerData0[24];		//Used in calculating the correlation scaler
+		unsigned long correlationScalerData1[24];		//
+		unsigned long correlationScaler;				//Correlation scaler between AIDA and other DAQs
+		unsigned long timeStampMSB;						//Most significant bit of the timestamp
+
+		unsigned int pauseItemCounter[24];
+		unsigned int resumeItemCounter[24];
+		unsigned int sync100Counter[24];
+
+		unsigned char dataType;		//Data type of data words
+
+		bool correlationStatus;		//Bool to keep track of whether correlation scaler has been measured
+		bool timeStampMSBStatus;	//Bool to keep track of whether timeStampMSB has been set
+
 		DataUnpacker::UnpackWords(std::pair < unsigned int, unsigned int> wordsIn)
 
 	public:
 		DataUnpacker();
 		~DataUnpacker(){};
 		void BeginDataUnpacker(DataReader & dataReader);
+
+		unsigned long GetTimeStampMSB();
+		unsigned char GetInfoCode();
+		unsigned char GetCorrScalerIndex();
+		unsigned char GetCorrScalerTimeStamp();
 
 };
 
