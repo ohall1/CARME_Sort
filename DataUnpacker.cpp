9672b5028eb2ad4a0725010f2b0d239c80bff6e5
ohall1 <ohall1@noreply.github.com>
2018-02-01 10:55:19 +0000

    Again problem with if statement

--- DataUnpacker.cpp
+++ DataUnpacker.cpp
@@ -65,7 +65,7 @@ bool DataUnpacker::UnpackWords(std::pair < unsigned int, unsigned int> wordsIn){
 
 		#ifdef OFFSETS
 		}
-		else{
+		else if(timestampMSBStatus){
 		#endif
 			adcDataItem.BuildTimestamp(timestampMSB);
 
